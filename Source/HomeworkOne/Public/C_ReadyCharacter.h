b3d052c2aa3a3d5993446077a4867113415378a9
anaego <anastas.ego@gmail.com>
2021-11-03 00:16:09 +0300

    Progress

---
+++ Source/HomeworkOne/Public/C_ReadyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HomeworkOneCharacter.h"
+#include "C_ReadyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOMEWORKONE_API AC_ReadyCharacter : public AHomeworkOneCharacter
+{
+	GENERATED_BODY()
+	
+};
